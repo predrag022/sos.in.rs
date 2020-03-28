ed3683dfd27749a8ecc2f2673a9db488ca07025c
predrag <pgajic@gmail.com>
2020-03-28 11:19:43 +0100

    misspelling fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,7 +17,7 @@
 
                     <div class="row">
                         <div class="panel-body">
-                            <h3>Vaša orgnizacija: {{$organization ?? ''}}</h3>
+                            <h3>Vaša organizacija: {{$organization ?? ''}}</h3>
                             <p></p>
                         </div>
                     </div>
